23bf7aff3cf1ab8d6021c05c71036cd3f7da66f8
Touchcreator <tochiokl62@gmail.com>
2025-04-08 18:56:39 -0400

    better ping commands

--- src/commands-ts/utility/ping.ts
+++ src/commands-ts/utility/ping.ts
@@ -1,12 +1,17 @@
-import { ChatInputCommandInteraction } from "discord.js";
-
+import { CacheType, ChatInputCommandInteraction } from "discord.js";
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
-        .setDescription("Ping test, or something"),
+        .setDescription("Ping test, but it's actually smart!"),
     async execute(interaction: ChatInputCommandInteraction) {
-        await interaction.reply("PONG!!!!");
+        await interaction.deferReply();
+
+        const reply = await interaction.fetchReply();
+
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+        interaction.editReply(`PONG!!!! (Took ${ping} milliseconds).`)
     },
 };
